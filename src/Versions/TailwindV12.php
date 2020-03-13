8e069239f74435e9831c775f5a30578e77e73494
Radoslaw Mejer <radmen@baethon.com>
2020-03-13 11:44:04 +0100

    Add string keys to patterns

--- src/Versions/TailwindV12.php
+++ src/Versions/TailwindV12.php
@@ -16,7 +16,7 @@ class TailwindV12 implements VersionInterface
     {
         return Utils::exportPatterns(
             [
-                [
+                'display' => [
                     'hidden', 'block', 'inline-block', 'inline',
                     'flex', 'inline-flex', 'grid',
                     'table',
@@ -29,192 +29,192 @@ class TailwindV12 implements VersionInterface
                     ]),
                     ...Utils::mix('table', ['column', 'footer', 'header', 'row'], '%s-%s-group'),
                 ],
-                ['box-border', 'box-content'],
-                Utils::mix('float', ['right', 'left', 'none']),
-                Utils::mix('clear', ['left', 'right', 'both']),
-                [
+                'box-sizing' => ['box-border', 'box-content'],
+                'float' => Utils::mix('float', ['right', 'left', 'none']),
+                'clear' => Utils::mix('clear', ['left', 'right', 'both']),
+                'object-fit' => [
                     ...Utils::mix('object', ['contain', 'cover', 'fill', 'none']),
                     'object-scale-down'
                 ],
-                [
+                'object-position' => [
                     ...Utils::mix('object', ['bottom', 'center', 'top', 'left', 'right']),
                     ...Utils::mix('object-left', ['bottom', 'top']),
                     ...Utils::mix('object-right', ['bottom', 'top']),
                 ],
-                Utils::mix('overflow', ['auto', 'hidden', 'visible', 'scroll']),
-                Utils::mix('overflow-x', ['auto', 'hidden', 'visible', 'scroll']),
-                Utils::mix('overflow-y', ['auto', 'hidden', 'visible', 'scroll']),
-                Utils::mix('scrolling', ['touch', 'auto']),
-                ['static', 'fixed', 'absolute', 'relative', 'sticky'],
-                Utils::mix('inset', ['0', 'auto']),
-                Utils::mix('inset-x', ['0', 'auto']),
-                Utils::mix('inset-y', ['0', 'auto']),
-                Utils::mix('top', ['0', 'auto']),
-                Utils::mix('right', ['0', 'auto']),
-                Utils::mix('bottom', ['0', 'auto']),
-                Utils::mix('left', ['0', 'auto']),
-                ['visible', 'invisible'],
-                ['r:z-\d+', 'z-auto'],
-                [
+                'overflow' => Utils::mix('overflow', ['auto', 'hidden', 'visible', 'scroll']),
+                'overflow-x' => Utils::mix('overflow-x', ['auto', 'hidden', 'visible', 'scroll']),
+                'overflox-y' => Utils::mix('overflow-y', ['auto', 'hidden', 'visible', 'scroll']),
+                'overflow-scrolling' => Utils::mix('scrolling', ['touch', 'auto']),
+                'position' => ['static', 'fixed', 'absolute', 'relative', 'sticky'],
+                'position-inset' => Utils::mix('inset', ['0', 'auto']),
+                'position-inset-x' => Utils::mix('inset-x', ['0', 'auto']),
+                'position-inset-y' => Utils::mix('inset-y', ['0', 'auto']),
+                'top' => Utils::mix('top', ['0', 'auto']),
+                'right' => Utils::mix('right', ['0', 'auto']),
+                'bottom' => Utils::mix('bottom', ['0', 'auto']),
+                'left' => Utils::mix('left', ['0', 'auto']),
+                'visibility' => ['visible', 'invisible'],
+                'z-index' => ['r:z-\d+', 'z-auto'],
+                'flex-direction' => [
                     'flex-row',
                     ...Utils::mix('flex', ['col', 'row']),
                     ...Utils::mix('flex', ['col', 'row'], '%s-%s-reverse'),
                 ],
-                ['flex-no-wrap', 'flex-wrap', 'flex-wrap-reverse'],
-                Utils::mix('items', ['stretch', 'start', 'center', 'end', 'baseline']),
-                Utils::mix('content', ['start', 'center', 'end', 'between', 'around']),
-                Utils::mix('self', ['auto', 'start', 'center', 'end', 'stretch']),
-                Utils::mix('justify', ['start', 'center', 'end', 'between', 'around']),
-                Utils::mix('flex', ['initial', '1', 'auto', 'none']),
-                ['flex-grow', 'flex-grow-0'],
-                ['flex-shrink', 'flex-shrink-0'],
-                [...Utils::mix('order', ['first', 'last', 'none']), 'r:order-\w+'],
-                ['grid-cols-none', 'r:grid-cols-\d+'],
-                ['col-auto', 'r:col-span-\d+'],
-                ['col-start-auto', 'r:col-start-\d+'],
-                ['col-end-auto', 'r:col-end-\d+'],
-                ['grid-rows-none', 'r:grid-rows-\d+'],
-                ['row-auto', 'r:row-span-\d+'],
-                ['row-start-auto', 'r:row-start-\d+'],
-                ['row-end-auto', 'r:row-end-\d+'],
-                ['gap-px', 'r:gap-\d+'],
-                ['row-gap-px', 'r:row-gap-\d+'],
-                ['col-gap-px', 'r:col-gap-\d+'],
-                [
+                'flex-wrap' => ['flex-no-wrap', 'flex-wrap', 'flex-wrap-reverse'],
+                'align-items' => Utils::mix('items', ['stretch', 'start', 'center', 'end', 'baseline']),
+                'align-content' => Utils::mix('content', ['start', 'center', 'end', 'between', 'around']),
+                'align-self' => Utils::mix('self', ['auto', 'start', 'center', 'end', 'stretch']),
+                'justify-content' => Utils::mix('justify', ['start', 'center', 'end', 'between', 'around']),
+                'flex' => Utils::mix('flex', ['initial', '1', 'auto', 'none']),
+                'flex-grow' => ['flex-grow', 'flex-grow-0'],
+                'flex-shrink' => ['flex-shrink', 'flex-shrink-0'],
+                'order' => [...Utils::mix('order', ['first', 'last', 'none']), 'r:order-\w+'],
+                'grid-template-columns' => ['grid-cols-none', 'r:grid-cols-\d+'],
+                'grid-column' => ['col-auto', 'r:col-span-\d+'],
+                'grid-column-start' => ['col-start-auto', 'r:col-start-\d+'],
+                'grid-column-end' => ['col-end-auto', 'r:col-end-\d+'],
+                'grid-template-rows' => ['grid-rows-none', 'r:grid-rows-\d+'],
+                'grid-row' => ['row-auto', 'r:row-span-\d+'],
+                'grid-row-start' => ['row-start-auto', 'r:row-start-\d+'],
+                'grid-row-end' => ['row-end-auto', 'r:row-end-\d+'],
+                'gap' => ['gap-px', 'r:gap-\d+'],
+                'row-gap' => ['row-gap-px', 'r:row-gap-\d+'],
+                'column-gap' => ['col-gap-px', 'r:col-gap-\d+'],
+                'grid-auto-flow' => [
                     ...Utils::mix('grid-flow', ['row', 'col']),
                     ...Utils::mix('grid-flow', ['row', 'col'], '%s-%s-dense'),
                 ],
-                ['p-px', 'r:p-\d+'],
-                ['px-px', 'r:px-\d+'],
-                ['py-px', 'r:py-\d+'],
-                ['pt-px', 'r:pt-\d+'],
-                ['pr-px', 'r:pr-\d+'],
-                ['pb-px', 'r:pb-\d+'],
-                ['pl-px', 'r:pl-\d+'],
-                ['m-auto', 'r:-?m-px', 'r:-?m-\d+'],
-                ['mx-auto', 'r:-?mx-\d+', 'r:-?mx-px'],
-                ['my-auto', 'r:-?my-px', 'r:-?my-\d+'],
-                ['mt-auto', 'r:-?mt-px', 'r:-?mt-\d+'],
-                ['mr-auto', 'r:-?mr-px', 'r:-?mr-\d+'],
-                ['mb-auto', 'r:-?mb-px', 'r:-?mb-\d+'],
-                ['ml-auto', 'r:-?ml-px', 'r:-?ml-\d+'],
-                [
+                'padding' => ['p-px', 'r:p-\d+'],
+                'padding-x' => ['px-px', 'r:px-\d+'],
+                'padding-y' => ['py-px', 'r:py-\d+'],
+                'padding-top' => ['pt-px', 'r:pt-\d+'],
+                'padding-right' => ['pr-px', 'r:pr-\d+'],
+                'padding-bottom' => ['pb-px', 'r:pb-\d+'],
+                'padding-left' => ['pl-px', 'r:pl-\d+'],
+                'margin' => ['m-auto', 'r:-?m-px', 'r:-?m-\d+'],
+                'margin-x' => ['mx-auto', 'r:-?mx-\d+', 'r:-?mx-px'],
+                'margin-y' => ['my-auto', 'r:-?my-px', 'r:-?my-\d+'],
+                'margin-top' => ['mt-auto', 'r:-?mt-px', 'r:-?mt-\d+'],
+                'margin-right' => ['mr-auto', 'r:-?mr-px', 'r:-?mr-\d+'],
+                'margin-bottom' => ['mb-auto', 'r:-?mb-px', 'r:-?mb-\d+'],
+                'margin-left' => ['ml-auto', 'r:-?ml-px', 'r:-?ml-\d+'],
+                'width' => [
                     ...Utils::mix('w', ['px', 'full', 'screen', 'auto']),
                     'r:w-\d+', 'r:w-\d+\/\d+'
                 ],
-                Utils::mix('min-w', ['0', 'full']),
-                [
+                'min-width' => Utils::mix('min-w', ['0', 'full']),
+                'max-width' => [
                     ...Utils::mix('max-w', ['xs', 'sm', 'md', 'lg', 'xl', 'full', 'none']),
                     ...Utils::mix('max-w-screen', ['sm', 'md', 'lg', 'xl']),
                     'r:max-w-\d+xl',
                 ],
-                [
+                'height' => [
                     ...Utils::mix('h', ['px', 'full', 'screen', 'auto']),
                     'r:h-\d+', 'r:h-\d+\/\d+',
                 ],
-                Utils::mix('min-h', ['0', 'full', 'screen']),
-                Utils::mix('max-h', ['full', 'screen']),
-                Utils::mix('font', ['sans', 'serif', 'mono']),
-                [
+                'min-height' => Utils::mix('min-h', ['0', 'full', 'screen']),
+                'max-height' => Utils::mix('max-h', ['full', 'screen']),
+                'font-family' => Utils::mix('font', ['sans', 'serif', 'mono']),
+                'font-size' => [
                     ...Utils::mix('text', ['xs', 'sm', 'base', 'lg', 'xl']),
                     'r:text-\d+xl',
                 ],
-                ['antialiased', 'subpixel-antialiased'],
-                ['italic', 'not-italic'],
-                Utils::mix('font', ['hairline', 'thin', 'light', 'normal', 'medium', 'semibold', 'bold', 'extrabold', 'black']),
-                Utils::mix('tracking', ['tighter', 'tight', 'normal', 'wide', 'wider', 'widest']),
-                [
+                'font-smoothing' => ['antialiased', 'subpixel-antialiased'],
+                'font-style' => ['italic', 'not-italic'],
+                'font-weight' => Utils::mix('font', ['hairline', 'thin', 'light', 'normal', 'medium', 'semibold', 'bold', 'extrabold', 'black']),
+                'letter-spacing' => Utils::mix('tracking', ['tighter', 'tight', 'normal', 'wide', 'wider', 'widest']),
+                'line-height' => [
                     ...Utils::mix('leading', ['none', 'tight', 'snug', 'normal', 'relaxed', 'loose']),
                     'r:leading-\d+',
                 ],
-                Utils::mix('list', ['none', 'disc', 'decimal']),
-                Utils::mix('list', ['inside', 'outside']),
-                [
+                'list-style-type' => Utils::mix('list', ['none', 'disc', 'decimal']),
+                'list-style-position' => Utils::mix('list', ['inside', 'outside']),
+                '::placeholder' => [
                     ...Utils::mix('placeholder', ['transparent', 'black', 'white']),
                     'r:placeholder-\w+-\d+',
                 ],
-                Utils::mix('text', ['left', 'center', 'right', 'justify']),
-                [
+                'text-align' => Utils::mix('text', ['left', 'center', 'right', 'justify']),
+                'color' => [
                     ...Utils::mix('text', ['transparent', 'black', 'white']),
                     'r:text-\w+-\d+',
                 ],
-                ['underline', 'no-underline', 'line-through'],
-                ['uppercase', 'lowercase', 'capitalize', 'normal-case'],
-                [
+                'text-decoration' => ['underline', 'no-underline', 'line-through'],
+                'text-transform' => ['uppercase', 'lowercase', 'capitalize', 'normal-case'],
+                'vertical-align' => [
                     ...Utils::mix('align', ['baseline', 'top', 'middle', 'bottom']),
                     ...Utils::mix('align-text', ['top', 'bottom']),
                 ],
-                Utils::mix('whitespace', ['normal', 'no-wrap', 'pre', 'pre-line', 'pre-wrap']),
-                [
+                'white-space' => Utils::mix('whitespace', ['normal', 'no-wrap', 'pre', 'pre-line', 'pre-wrap']),
+                'word-break' => [
                     ...Utils::mix('break', ['normal', 'words', 'all']),
                     'truncate'
                 ],
-                Utils::mix('bg', ['fixed', 'local', 'scroll']),
-                [
+                'background-attachment' => Utils::mix('bg', ['fixed', 'local', 'scroll']),
+                'background-color' => [
                     ...Utils::mix('bg', ['transparent', 'black', 'white']),
                     'r:bg-\w+-\d+',
                 ],
-                [
+                'background-position' => [
                     ...Utils::mix('bg', ['bottom', 'center', 'left', 'right', 'top']),
                     ...Utils::mix('bg-left', ['bottom', 'top']),
                     ...Utils::mix('bg-right', ['bottom', 'top']),
                 ],
-                [
+                'background-repeat' => [
                     ...Utils::mix('bg-repeat', ['x', 'y', 'space', 'round']),
                     'bg-repeat', 'bg-no-repeat',
                 ],
-                Utils::mix('bg', ['auto', 'cover', 'contain']),
-                Utils::mix('border', ['solid', 'dashed', 'dotted', 'double', 'none']),
-                ['border', 'r:border-\d+'],
-                ['border-t', 'r:border-t-\d+'],
-                ['border-r', 'r:border-r-\d+'],
-                ['border-b', 'r:border-b-\d+'],
-                ['border-l', 'r:border-l-\d+'],
-                [
+                'background-size' => Utils::mix('bg', ['auto', 'cover', 'contain']),
+                'border-style' => Utils::mix('border', ['solid', 'dashed', 'dotted', 'double', 'none']),
+                'border-width' => ['border', 'r:border-\d+'],
+                'border-width-top' => ['border-t', 'r:border-t-\d+'],
+                'border-width-right' => ['border-r', 'r:border-r-\d+'],
+                'border-width-bottom' => ['border-b', 'r:border-b-\d+'],
+                'border-width-left' => ['border-l', 'r:border-l-\d+'],
+                'border-color' => [
                     ...Utils::mix('border', ['transparent', 'black', 'white']),
                     'r:border-\w+-\d+',
                 ],
-                [
+                'border-radius' => [
                     ...Utils::mix('rounded', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded'
                 ],
-                [
+                'border-top-radius' => [
                     ...Utils::mix('rounded-t', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-t',
                 ],
-                [
+                'border-right-radius' => [
                     ...Utils::mix('rounded-r', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-r',
                 ],
-                [
+                'border-bottom-radius' => [
                     ...Utils::mix('rounded-b', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-b',
                 ],
-                [
+                'border-left-radius' => [
                     ...Utils::mix('rounded-l', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-l',
                 ],
-                [
+                'border-top-left-radius' => [
                     ...Utils::mix('rounded-tl', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-tl',
                 ],
-                [
+                'border-top-right-radius' => [
                     ...Utils::mix('rounded-tr', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-tr',
                 ],
-                [
+                'border-bottom-left-radius' => [
                     ...Utils::mix('rounded-bl', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-bl',
                 ],
-                [
+                'border-bottom-right-radius' =>[
                     ...Utils::mix('rounded-br', ['none', 'sm', 'md', 'lg', 'full']),
                     'rounded-br',
                 ],
-                Utils::mix('border', ['collapse', 'separate']),
-                Utils::mix('table', ['auto', 'fixed']),
-                ['shadow', 'r:shadow-\w+'],
-                ['r:opacity-\d+'],
-                [
+                'border-collapse' => Utils::mix('border', ['collapse', 'separate']),
+                'table-layout' => Utils::mix('table', ['auto', 'fixed']),
+                'box-shadow' => ['shadow', 'r:shadow-\w+'],
+                'opacity' => ['r:opacity-\d+'],
+                'transition-property' => [
                     ...Utils::mix('transition', [
                         'none',
                         'all',
@@ -225,18 +225,18 @@ class TailwindV12 implements VersionInterface
                     ]),
                     'transition',
                 ],
-                ['r:duration-\d+'],
-                Utils::mix('ease', ['linear', 'in', 'out', 'in-out']),
-                ['r:scale-\d+'],
-                ['r:scale-x-\d+'],
-                ['r:scale-y-\d+'],
-                ['r:-?rotate-\d+'],
-                ['r:-?translate-x-\d+', 'r:-?translate-x-(full|px)', 'r:-?translate-x-1\/2'],
-                ['r:-?translate-y-\d+', 'r:-?translate-y-(full|px)', 'r:-?translate-y-1\/2'],
-                ['r:-?skew-x-\d+'],
-                ['r:-?skew-y-\d+'],
-                Utils::mix('origin', ['center', 'top', 'top-right', 'right', 'bottom-right', 'bottom', 'bottom-left', 'left', 'top-left']),
-                Utils::mix('cursor', [
+                'transition-duration' => ['r:duration-\d+'],
+                'transition-timing-function' => Utils::mix('ease', ['linear', 'in', 'out', 'in-out']),
+                'transform-scale' => ['r:scale-\d+'],
+                'transform-scale-x' => ['r:scale-x-\d+'],
+                'transform-scale-y' => ['r:scale-y-\d+'],
+                'transform-rotate' => ['r:-?rotate-\d+'],
+                'transform-translate-x' => ['r:-?translate-x-\d+', 'r:-?translate-x-(full|px)', 'r:-?translate-x-1\/2'],
+                'transform-translate-y' => ['r:-?translate-y-\d+', 'r:-?translate-y-(full|px)', 'r:-?translate-y-1\/2'],
+                'transform-skew-x' => ['r:-?skew-x-\d+'],
+                'transform-skew-y' => ['r:-?skew-y-\d+'],
+                'transform-origin' => Utils::mix('origin', ['center', 'top', 'top-right', 'right', 'bottom-right', 'bottom', 'bottom-left', 'left', 'top-left']),
+                'cursor' => Utils::mix('cursor', [
                     'auto',
                     'default',
                     'pointer',
@@ -245,14 +245,14 @@ class TailwindV12 implements VersionInterface
                     'move',
                     'not-allowed',
                 ]),
-                ['pointer-events-auto', 'pointer-events-none'],
-                [
+                'pointer-events' => ['pointer-events-auto', 'pointer-events-none'],
+                'resize' => [
                     ...Utils::mix('resize', ['none', 'x', 'y']),
                     'resize',
                 ],
-                Utils::mix('select', ['none', 'text', 'all', 'auto']),
-                ['r:stroke-\d+'],
-                ['sr-only', 'not-sr-only'],
+                'user-select' => Utils::mix('select', ['none', 'text', 'all', 'auto']),
+                'stroke-width' => ['r:stroke-\d+'],
+                'screen-readers' => ['sr-only', 'not-sr-only'],
             ],
             static::getPrefixes(),
         );
